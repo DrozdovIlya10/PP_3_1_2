edbe02a39e1f892cc9f6aefaf1acdd0435febb37
DrozdovIlya10 <Drozdov.ilya4610@gmail.com>
2022-11-08 13:28:10 +0700

    2.3.1_Fix

--- PP231/src/main/java/web/dao/UserDaoImp.java
+++ PP231/src/main/java/web/dao/UserDaoImp.java
@@ -1,12 +1,7 @@
 package web.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Repository;
-
-import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
-
 
 import javax.persistence.*;
 
@@ -18,28 +13,25 @@ public class UserDaoImp implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
     @Override
     public void add(User user) {
         entityManager.persist(user);
         entityManager.flush();
     }
 
-    @Transactional
     @Override
     public void delete(Long id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
     }
 
-    @Transactional
     @Override
     @SuppressWarnings("unchecked")
     public List<User> listUsers() {
         List<User> list = entityManager.createQuery("select user from User user").getResultList();
         return list;
     }
-    @Transactional
+
     @Override
     public User showUser(long id) {
         User user = entityManager.find(User.class, id);
@@ -47,16 +39,9 @@ public class UserDaoImp implements UserDao {
         return user;
     }
 
-    @Transactional
     @Override
     public void update(long id, User user2) {
-        User user = showUser(id);
-        //entityManager.detach(user);
-        user.setFirstName(user2.getFirstName());
-        user.setLastName(user2.getLastName());
-        user.setEmail(user2.getEmail());
-        entityManager.merge(user);
-        entityManager.flush();
+        entityManager.merge(user2);
     }
 
 }
